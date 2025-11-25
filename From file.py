375b69d7e8258cd55a9bad232c9b2ce18850fa2c
aliss <ac3721@student.ubc.ca>
2025-11-24 17:50:10 -0800

    Make parsing more robust and add support for multiple lines of text

--- From file.py
+++ From file.py
@@ -10,10 +10,23 @@ def parse_entries(filename, vtt = False):
 
     for entry in entries:
         lines = [line.strip() for line in entry.splitlines() if line.strip()]
+        print(lines)
         if len(lines) >= 7:
-            start = f"{lines[0]}:{lines[3]},{lines[6]}"
-            text = lines[9]
-            end = f"{lines[10]}:{lines[13]},{lines[16]}"
+            index = lines.index('Set to current time')
+            start = f"{lines[index-8]}:{lines[index-5]},{lines[index-2]}"
+            lines = lines[(index+1):]
+            print("second\n", lines)
+            second_index = lines.index(':')
+            text = lines[:(second_index - 2)]
+            end = f"{lines[second_index - 2]}:{lines[second_index + 1]},{lines[second_index + 4]}"
+            if len(text) > 1:
+                combined = [""]
+                for line in text:
+                    combined[0] += line + '\n'
+                print (combined[0])
+                text = combined[0][:len(combined[0])-1]     
+            else:
+                text = text[0]
             if vtt:
                 results.append([start.replace(',','.'),text,end.replace(',','.')])
             else:
